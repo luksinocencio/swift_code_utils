070a65effdfd27336d80cfb02f68e7a3d6218516
luksinocencio <luksinocencio@gmail.com>
2023-10-03 21:23:39 -0300

    feat: create shimmer example

---
+++ ShimmerFun/ShimmerFun/Game.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct Game {
+    let title: String
+    let year: String
+    init(_ name: String, _ year: String) {
+        self.title = name
+        self.year = year
+    }
+}
---
+++ ShimmerFun/ShimmerFun/UIColor+Extensions.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIColor {
+    static var gradientLightGrey: UIColor {
+        UIColor(red: 239/255, green: 241/255, blue: 241/255, alpha: 1)
+    }
+    
+    static var gradientDarkGrey: UIColor {
+        UIColor(red: 201/255, green: 201/255, blue: 201/255, alpha: 1)
+    }
+}
